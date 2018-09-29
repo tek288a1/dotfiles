97d514ea9ed04231eb2fe5c7aef1c999aa70621b
tae <tek288a1@gmail.com>
2018-09-29 19:06:15 -0400

    powerline fonts in AUR

--- scripts/W530_02_fonts.sh
+++ scripts/W530_02_fonts.sh
@@ -9,7 +9,7 @@ sudo pacman -S --noconfirm --needed noto-fonts
 sudo pacman -S --noconfirm --needed gnome-font-viewer
 sudo pacman -S --noconfirm --needed ttf-font-awesome
 sudo pacman -S --noconfirm --needed otf-font-awesome
-sudo pacman -S --noconfirm --needed powerline-fonts
+yaourt -S --noconfirm powerline-fonts-git
 #sudo pacman -R --noconfirm powerline-fonts
 
 ## update cache
