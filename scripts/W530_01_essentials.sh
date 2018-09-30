ad673e8eb53a4217388751d705bd4c2d2997c330
tae <tek288a1@gmail.com>
2018-09-29 21:58:19 -0400

    installation script added

---
+++ scripts/W530_01_essentials.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+sudo pacman -S --noconfirm --needed emacs
+sudo pacman -S --noconfirm --needed rxvt-unicode rxvt-unicode-terminfo
+sudo pacman -S --noconfirm --needed chromium
+sudo pacman -S --noconfirm --needed rofi
+sudo pacman -S --noconfirm --needed tmux screen
+sudo pacman -S --noconfirm --needed aspell-en
+sudo pacman -S --noconfirm --needed htop glances gtop
+sudo pacman -S --noconfirm --needed screenfetch archey3 neofetch
+sudo pacman -S --noconfirm --needed unzip tree
