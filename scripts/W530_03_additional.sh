557176f4ddcb474732900af18f819e4296abd70b
tae <tek288a1@gmail.com>
2018-10-14 00:29:48 -0400

    added new scripts

--- scripts/W530_03_additional.sh
+++ scripts/W530_03_additional.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
+sudo pacman -S --noconfirm --needed ibus ibus-qt ibus-hangul # for korean input
 yaourt -S --noconfirm insync
 yaourt -S --noconfirm dropbox
 yaourt -S --noconfirm mailspring
-yaourt -S --noconfirm laptop-mode-tools
-sudo pacman -S --noconfirm --needed ibus ibus-qt ibus-hangul # for korean input
+yaourt -S --noconfirm albert
+# yaourt -S --noconfirm laptop-mode-tools # conflict with tlp
