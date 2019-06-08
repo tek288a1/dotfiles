04993572260b98bc6cd65de01721f02848317e1b
tae <tek288a1@gmail.com>
2019-06-08 13:02:23 -0400

    X11 related files, OMZ installation and .zshrc file, and some bash scripts are added

---
+++ omz/install_omz.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+sudo pacman -S --noconfirm --needed zsh
+sudo pacman -S --noconfirm --needed zsh-syntax-highlighting
+yay -S --noconfirm --needed command-not-found
+
+# Install Oh My Zshell
+wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+
+# Download powerlevel9k theme
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+mv $HOME/.zshrc $HOME/.zshrc.bak
+cp ./.zshrc $HOME/.zshrc
+
+## For proper theming, make sure to copy .Xresources file from tkconf/X11 directory.
