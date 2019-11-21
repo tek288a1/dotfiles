23ccc765830d88c0ec3c8ed447437bffcbdfef28
Tae Eun Kim <tek288a1@gmail.com>
2019-11-21 08:17:38 -0500

    .gitignore elpa directory

--- emacs/bare/elpa/use-package-ensure-system-package-20180913.1501/use-package-ensure-system-package-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "use-package-ensure-system-package" "20180913.1501" "auto install system packages" '((use-package "2.1") (system-packages "1.0.4")) :commit "df2528cd5116cdf2885f6f497511bf0946fe0825" :keywords '("convenience" "tools" "extensions") :authors '(("Justin Talbott" . "justin@waymondo.com")) :maintainer '("Justin Talbott" . "justin@waymondo.com") :url "https://github.com/waymondo/use-package-ensure-system-package")
