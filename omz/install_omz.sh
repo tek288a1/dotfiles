94fb4536bcc408372ef5d62db97301a27352fdd7
Tae Eun Kim <tek288a1@gmail.com>
2020-01-26 20:29:24 -0500

    install omz script

--- omz/install_omz.sh
+++ omz/install_omz.sh
@@ -4,14 +4,16 @@ set -e
 sudo pacman -S --noconfirm --needed zsh
 sudo pacman -S --noconfirm --needed zsh-syntax-highlighting
 sudo pacman -S --noconfirm --needed neofetch
-#yay -S --noconfirm --needed command-not-found
 yay -S --noconfirm --needed nerd-fonts-hack
 
 # Install Oh My Zshell
 wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
 
-# Download powerlevel9k theme
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+
+# Download powerlevel9k/10k theme
+git clone https://github.com/bhilburn/powerlevel9k.git $ZSH_CUSTOM/themes/powerlevel9k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
 mv $HOME/.zshrc $HOME/.zshrc.bak
 cp ./.zshrc $HOME/.zshrc
