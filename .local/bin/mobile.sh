1978182bc5fc92c11e6f01117310cee6895798ad
Tae Eun Kim <tek288a1@gmail.com>
2020-11-15 03:13:14 -0500

    display setting scripts mods

--- .local/bin/mobile.sh
+++ .local/bin/mobile.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off
-setbg 
+xrandr\
+    --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal\
+    --output DP-1 --off\
+    --output DP-1-1 --off\
+    --output DP-1-2 --off\
+    --output DP-2 --off\
+    --output DP-2-1 --off\
+    --output DP-2-2 --off\
+    --output DP-3 --off
+setbg && refbar && sleep 1
+notify-send.sh "💻 Laptop Screen Only"
--- .local/bin/three_monitors.sh
+++ .local/bin/three_monitors.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1003x1080 --rotate normal --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-3 --off 
-setbg
+xrandr\
+    --output eDP-1 --primary --mode 1920x1080 --pos 1003x1080 --rotate normal\
+    --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal\
+    --output DP-2 --mode 1920x1080 --pos 1920x0 --rotate normal\
+    --output DP-3 --off
+setbg && refbar && sleep 1
+notify-send.sh "💻 Connected to 2 Monitors"
